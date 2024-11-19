dd0051d9fad52f02fad34ff8bf2cc0f45936a6ad
Sandipsinh Dilipsinh Rathod <62684960+ssddOnTop@users.noreply.github.com>
2024-11-19 01:09:49 -0500

    chore: add ci (#3)

---
+++ tests/ci.rs
@@ -0,0 +1,51 @@
+use gh_workflow::toolchain::Toolchain;
+use gh_workflow::*;
+
+#[test]
+fn generate() {
+    let flags = RustFlags::deny("warnings");
+
+    let build = Job::new("Build and Test")
+        .add_step(Step::checkout())
+        .add_step(
+            Toolchain::default()
+                .add_stable()
+                .add_nightly()
+                .add_clippy()
+                .add_fmt(),
+        )
+        .add_step(
+            Cargo::new("test")
+                .args("--all-features --workspace")
+                .name("Cargo Test"),
+        )
+        .add_step(
+            Cargo::new("fmt")
+                .nightly()
+                .args("--check")
+                .name("Cargo Fmt"),
+        )
+        .add_step(
+            Cargo::new("clippy")
+                .nightly()
+                .args("--all-features --workspace -- -D warnings")
+                .name("Cargo Clippy"),
+        );
+
+    let event = Event::default()
+        .push(Push::default().add_branch("main"))
+        .pull_request_target(
+            PullRequestTarget::default()
+                .add_type(PullRequestType::Opened)
+                .add_type(PullRequestType::Synchronize)
+                .add_type(PullRequestType::Reopened)
+                .add_branch("main"),
+        );
+
+    Workflow::new("Build and Test")
+        .add_env(flags)
+        .on(event)
+        .add_job("build", build)
+        .generate()
+        .unwrap();
+}
