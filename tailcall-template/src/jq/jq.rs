520d61df048de77fe3ac5d15cd18709150dfadc8
Panagiotis <panagiotiskaratakis@gmail.com>
2024-11-28 12:25:04 +0000

    impl `from_map`

--- tailcall-template/src/jq/jq.rs
+++ tailcall-template/src/jq/jq.rs
@@ -1,14 +1,9 @@
 use jaq_core::ValR;
 
-use crate::jsonlike::JsonLike;
+use crate::jsonlike::{JsonLike, JsonObjectLike};
 
 #[derive(Clone, PartialEq, PartialOrd)]
-pub struct JsonLikeHelper<A>
-where
-    A: for<'a> JsonLike<'a>,
-{
-    pub data: A,
-}
+pub struct JsonLikeHelper<A: for<'a> JsonLike<'a>>(pub A);
 
 impl<A> jaq_core::ValT for JsonLikeHelper<A>
 where
@@ -16,13 +11,27 @@ where
 {
     fn from_num(n: &str) -> ValR<Self> {
         match n.parse::<f64>() {
-            Ok(num) => ValR::Ok(JsonLikeHelper { data: A::number_f64(num) }),
+            Ok(num) => ValR::Ok(JsonLikeHelper(A::number_f64(num))),
             Err(err) => ValR::Err(jaq_core::Error::str(format!("Invalid number format: {}", err.to_string()))),
         }
     }
 
     fn from_map<I: IntoIterator<Item = (Self, Self)>>(iter: I) -> ValR<Self> {
-        todo!()
+        iter.into_iter().fold(ValR::Ok(Self(JsonLike::object(JsonObjectLike::new()))), |acc, (key, value)| {
+            let key = match JsonLike::as_str(&key.0) {
+                Some(key) => key,
+                None => return ValR::Err(jaq_core::Error::str("The value cannot be converted to String")),
+            };
+
+            match acc {
+                Ok(mut acc) => {
+                    let acc_mut = JsonLike::as_object_mut(&mut acc.0).unwrap();
+                    acc_mut.insert_key(key, value.0);
+                    ValR::Ok(acc)
+                },
+                Err(err) => ValR::Err(err),
+            }
+        })
     }
 
     fn values(self) -> Box<dyn Iterator<Item = ValR<Self>>> {
