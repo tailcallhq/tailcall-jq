92d412135da3ac3f6ffca7ac6bdacefd5a6c7eb4
Mehul Prajapati <mehulprajapati2802@gmail.com>
2024-12-14 19:36:49 -0500

    feat: migrate to gh-workflow-tailcall (#10)

--- tests/ci.rs
+++ tests/ci.rs
@@ -1,52 +1,8 @@
-use gh_workflow::toolchain::Toolchain;
-use gh_workflow::*;
+use gh_workflow_tailcall::*;
 
 #[test]
-fn generate() {
-    let flags = RustFlags::deny("warnings");
+fn generate_ci_workflow() {
+    let workflow = Workflow::default().auto_release(true).benchmarks(true);
 
-    let build = Job::new("Build and Test")
-        .add_step(Step::checkout())
-        .add_step(
-            Toolchain::default()
-                .add_stable()
-                .add_nightly()
-                .add_clippy()
-                .add_fmt(),
-        )
-        .add_step(
-            Cargo::new("test")
-                .args("--all-features --workspace")
-                .name("Cargo Test"),
-        )
-        .add_step(Cargo::new("bench").name("Cargo Bench"))
-        .add_step(
-            Cargo::new("fmt")
-                .nightly()
-                .args("--check")
-                .name("Cargo Fmt"),
-        )
-        .add_step(
-            Cargo::new("clippy")
-                .nightly()
-                .args("--all-features --workspace -- -D warnings")
-                .name("Cargo Clippy"),
-        );
-
-    let event = Event::default()
-        .push(Push::default().add_branch("main"))
-        .pull_request(
-            PullRequest::default()
-                .add_type(PullRequestType::Opened)
-                .add_type(PullRequestType::Synchronize)
-                .add_type(PullRequestType::Reopened)
-                .add_branch("main"),
-        );
-
-    Workflow::new("Build and Test")
-        .add_env(flags)
-        .on(event)
-        .add_job("build", build)
-        .generate()
-        .unwrap();
+    workflow.generate().unwrap();
 }
